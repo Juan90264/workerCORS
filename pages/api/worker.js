bf715acf0134526b189ff10c3961b03c6a54bbe3
Juan90264 <68405238+Juan90264@users.noreply.github.com>
2025-06-26 05:34:13 -0300

    Update worker.js

--- pages/api/worker.js
+++ pages/api/worker.js
@@ -18,6 +18,7 @@ export default async function handler(req, res) {
   }
 
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Cache-Control');
 
   const targetUrl = req.query.url;
